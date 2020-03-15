5f8045a860ed4775a24f8d24db4044b75b12f0fd
Marina <alcubilla.marina@gmail.com>
2020-03-14 22:04:50 -0600

    update

---
+++ Data/users.js
@@ -0,0 +1,21 @@
+let USERS = [
+    {
+        status: 1,
+        USER: 'marina',
+        PASSWORD: '$2y$12$FqxbWI20IYWJ6oTB4i7dZel8x/ZrqTICggcAvw3w3GS1h2HhwxCQ.', //gato
+       
+    }, 
+
+    {
+        status: 0,
+        USER: 'jesus',
+        PASSWORD: '$2y$12$8eXSyxNX7Bkij0paf8hE6efWbYkcw.XZp0auIQazJHoUg.kiQH6gW', //perro
+     
+    },
+    {
+        status: 1,
+        USER: 'andrea',
+        PASSWORD: '$2y$12$Zr5XaBUqmABS63e7D8kCTel.rFeSxfrwmqnwXEPht/vJHFB/WhLm6', //hola
+     
+    }
+]
